55ab6fe826bcaddd4e7d4a7dd6db54e3ee0ba461
Jack Matthews <jackhkmatthews@gmail.com>
2024-06-01 18:10:01 +0100

    init

---
+++ app/textClasses.ts
@@ -0,0 +1,6 @@
+export const textClasses = {
+  large: "text-3xl font-sans leading-tight",
+  medium: "text-xl font-sans leading-tight",
+  small: "text-md font-sans leading-tight",
+  link: "underline underline-offset-2 text-sky-700 hover:text-sky-950 cursor-pointer focus:bg-slate-300 outline-none transition-colors",
+} as const;
