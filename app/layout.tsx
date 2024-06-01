802fe4d370ca378fd0f70c2f1e7d0570e42173b8
Jack Matthews <jackhkmatthews@gmail.com>
2024-06-02 00:06:30 +0100

    feat(text): update colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full bg-gray-50 dark:bg-gray-950">
       <body className="min-h-full h-full grid">{children}</body>
     </html>
   );
--- app/textClasses.ts
+++ app/textClasses.ts
@@ -2,5 +2,5 @@ export const textClasses = {
   large: "text-3xl font-sans leading-tight",
   medium: "text-xl font-sans leading-tight",
   small: "text-md font-sans leading-tight",
-  link: "underline underline-offset-2 text-sky-700 hover:text-sky-950 cursor-pointer focus:bg-slate-300 outline-none transition-colors",
+  link: "underline underline-offset-4 text-sky-700 hover:text-sky-900 cursor-pointer focus:bg-sky-200 outline-none transition-colors dark:hover:text-sky-500 dark:focus:bg-sky-950",
 } as const;
