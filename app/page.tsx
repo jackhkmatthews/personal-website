d1abb421290adee94948fec156be684a2a529795
Jack Matthews <jackhkmatthews@gmail.com>
2024-11-22 22:20:39 +0000

    fix(color): green

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     >
       <h1
         className={twJoin(
-          "font-sans leading-tight text-red-700 text-[max(1.875rem,19cqw)] translate-x-[-0.1ch]"
+          "font-sans leading-tight text-green-700 text-[max(1.875rem,19cqw)] translate-x-[-0.1ch]"
         )}
       >
         Hi, I&apos;m{" "}
