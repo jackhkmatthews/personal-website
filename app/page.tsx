afa34b705c66a831a2a86e4eb4f8f60410735526
Jack Matthews <jackhkmatthews@gmail.com>
2025-08-18 17:14:26 +0100

    build(next): update to 15

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
     <main
       className={twJoin(
         textClasses.medium,
-        "p-4 pt-8 md:px-[max(1rem,10vw)] md:py-[max(1rem,20vh)] flex flex-col gap-4 break-words [word-break:break-word] @container max-w-[40ch] box-content"
+        "p-4 pt-8 md:px-[max(1rem,10vw)] md:py-[max(1rem,20vh)] flex flex-col gap-4 break-words [word-break:break-word] @container max-w-[40ch] box-content",
       )}
     >
       <h1
         className={twJoin(
-          "font-sans leading-tight text-sky-700 text-[max(1.875rem,19cqw)] translate-x-[-0.1ch]"
+          "font-sans leading-tight text-sky-700 text-[max(1.875rem,19cqw)] translate-x-[-0.1ch]",
         )}
       >
         Hi, I&apos;m{" "}
